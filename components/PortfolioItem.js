df61bd570df3fd7dbe162a33d93f733dd0405036
Santhoshkrishnachaitanya Chelikavada <everythingskc@gmail.com>
2019-10-30 23:09:55 -0700

    fixed portfolio bug

--- components/PortfolioItem.js
+++ components/PortfolioItem.js
@@ -3,43 +3,87 @@ import Router from "next/router";
 import Card from "../components/Card";
 import "../styles/styles.css";
 import "../styles/cards.css";
+import firebase from "./firebase";
+
 
 class PortfolioItem extends Component {
   constructor(props) {
     super(props);
     this.dataItem = this.props.dataItem;
     this.viewSubmission = this.viewSubmission.bind(this);
+    this.state = {
+      user: null
+    };
   }
 
-  viewSubmission(event,submissionLink) {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user: user });
+      }
+    });
+  }
+
+  viewSubmission(event, submissionLink) {
     event.preventDefault();
   }
   render() {
-    return (
-      <div className="card" style={{width:"75%",display:"flex",flexDirection:"column",padding:"16px"}}>
-        <div className="card-heading">{this.dataItem["challenge_title"]}</div>
+    let dataInfo;
 
-        <div
-          style={{ color: "blue" }}
-          className="card-subheading"
-          onClick={(event)=>this.viewSubmission(event,this.dataItem["submission_link"])}
-        >
-          View Submission
-        </div>
 
-        <div className="paragraph-small">
-          Scores:
-          <br />
-          <br />
-          Innovativeness - {this.dataItem.innv}<br/>
-          Product Design - {this.dataItem["prod_design"]}<br/>
-          Feasibility - {this.dataItem.feasibility}<br/>
-          Market Research and Competitive Analysis -{" "}
-          {this.dataItem["market_research"]}<br/>
-          Launch Plan - {this.dataItem["launch_plan"]}<br/>
-        </div>
-      </div>
-    );
+    if(this.state.user) {
+        if (this.props.user.uid === this.state.user.uid) {
+            dataInfo = (
+                <div className="paragraph-small">
+                    Scores:
+                    <br/>
+                    <br/>
+                    Customer Analysis - {this.dataItem.customer_analysis}
+                    <br/>
+                    Product Design - {this.dataItem["prod_design"]}
+                    <br/>
+                    Feasibility - {this.dataItem.feasibility}
+                    <br/>
+                    Competitive Strategy - {this.dataItem["competitive_strategy"]}
+                    <br/>
+                    Launch Plan - {this.dataItem["launch_plan"]}
+                    <br/>
+                </div>
+            );
+        } else {
+            dataInfo = <div></div>;
+        }
+
+
+        return (
+            <div
+                className="card"
+                style={{
+                    width: "75%",
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "16px"
+                }}
+            >
+                <div className="card-heading">{this.dataItem["challenge_title"]}</div>
+
+                <div
+                    style={{color: "blue"}}
+                    className="card-subheading"
+                    onClick={event =>
+                        this.viewSubmission(event, this.dataItem["submission_link"])
+                    }
+                >
+                    View Submission
+                </div>
+
+                {dataInfo}
+            </div>
+        );
+    }
+    else{
+        return <div/>
+    }
   }
 }
 export default PortfolioItem;
--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -4,114 +4,114 @@ import "../../styles/styles.css";
 import Navbar from "../../components/nav";
 import Router from "next/router";
 import PortfolioItem from "../../components/PortfolioItem";
-
+import firebase from "../../components/firebase";
 
 class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      id: ""
+      id: "",
+      user: null,
+      challengeData: null
     };
-    this.challengeData = [];
     this.getChallengeData.bind(this);
   }
 
-  anonymizeData(challengeData){
+  anonymizeData(challengeData) {}
 
+  async getChallengeData(id) {
+    firebase
+      .firestore()
+      .collection("Portfolios")
+      .where("username", "==", id)
+      .get()
+      .then(docs => {
+        console.log(docs);
 
-
-
-  }
-
-  getChallengeData(id) {
-    const challenges = [
-      {
-        challenge_title: "The Binance Challenge",
-        innv: "7/7",
-        prod_design: "6/7",
-        feasibility: "7/7",
-        market_research: "6/7",
-        competitive_analysis: "7/7",
-        launch_plan: "6/7",
-        submission_link: "https://google.com"
-      },
-      {
-        challenge_title: "The Coding Bootcamp Challenge",
-        innv: "7/7",
-        prod_design: "6/7",
-        feasibility: "7/7",
-        market_research: "6/7",
-        competitive_analysis: "7/7",
-        launch_plan: "6/7",
-        submission_link: "https://google.com"
-      }
-    ];
-
-    return challenges;
+        let arr = [];
+        docs.forEach(function(doc){
+          arr.push(doc.data())
+        })
+        this.setState({
+          challengeData: arr[0].challenges
+        });
+      });
   }
 
   componentDidMount() {
-
-
-
     let url = window.location.href.split("/");
     console.log(url);
     let id = url[url.length - 1];
     console.log(id);
-    this.setState({
-      id: id
-    });
 
-    this.challengeData = this.getChallengeData(this.state.id);
+    let self = this;
+    self.getChallengeData(id);
+    firebase.auth().onAuthStateChanged(function(user) {
+      self.setState({
+        id: id,
+        user: user
+      });
+    });
   }
 
   render() {
-    console.log(this.challengeData);
+    console.log("Challenge Data", this.state.challengeData);
+    console.log("Username", this.state.id);
     let challengeComponents = [];
-    for (let i = 0; i < this.challengeData.length - 1; i += 2) {
-      challengeComponents.push(
-        <div
-          className="row"
-          style={{
-            width: "100%",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <div className="col-50" style={{ alignItems: "center" }}>
-            <PortfolioItem
-              key={this.challengeData[i]["submission_link"]}
-              dataItem={this.challengeData[i]}
-            />
+
+    if (this.state.challengeData) {
+      for (let i = 0; i < this.state.challengeData.length; i ++) {
+        challengeComponents.push(
+          <div
+            className="row"
+            style={{
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <div className="col-50" style={{ alignItems: "center" }}>
+              <PortfolioItem
+                user={this.state.user}
+                key={this.state.challengeData[i]["submission_link"]}
+                dataItem={this.state.challengeData[i]}
+              />
+            </div>
+
           </div>
-          <div className="col-50" style={{ alignItems: "center" }}>
-            <PortfolioItem
-              key={this.challengeData[i + 1]["submission_link"]}
-              dataItem={this.challengeData[i + 1]}
-            />
-          </div>
-        </div>
-      );
+        );
+      }
     }
 
-    return (
-      <>
-        <Navbar />
+    let retVal;
+    if (this.state.id && this.state.challengeData) {
+      retVal = (
+        <>
+          <Navbar />
 
-        <div
-          className="container"
-          style={{
-            display: "flex",
-            flexDirection: "column",
+          <div
+            className="container"
+            style={{
+              display: "flex",
+              flexDirection: "column",
 
-            alignItems: "center"
-          }}
-        >
-          <div className="heading-2" style={{marginTop:"5vh",marginBottom:"5vh"}}>{this.state.id}'s Portfolio</div>
-          {challengeComponents}
-        </div>
-      </>
-    );
+              alignItems: "center"
+            }}
+          >
+            <div
+              className="heading-2"
+              style={{ marginTop: "5vh", marginBottom: "5vh" }}
+            >
+              {this.state.id}'s Portfolio
+            </div>
+            {challengeComponents}
+          </div>
+        </>
+      );
+    } else {
+      retVal = <div />;
+    }
+    return retVal;
   }
 }
 
